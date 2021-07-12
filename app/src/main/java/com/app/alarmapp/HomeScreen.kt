8a6c4901bf57e7a565ef6d223e598f328821973a
Erik Dubois <erik.dubois@gmail.com>
2021-07-12 20:52:28 +0200

    Init

---
+++ app/src/main/java/com/app/alarmapp/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.app.alarmapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.alarmapp.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen)
+    }
+}
